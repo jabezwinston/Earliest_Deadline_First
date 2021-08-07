79aa5fbe74c1920eddb3f32bddf63981201a6156
Jabez Winston <jabezwinston@gmail.com>
2021-08-07 14:40:17 +0530

    Formatted code

--- func_def.c
+++ func_def.c
@@ -4,96 +4,94 @@
  *  Created on: 27-Jan-2016
  *      Author: winston
  */
-#include<stdio.h>
-#include"edf.h"
+#include <stdio.h>
+#include "edf.h"
 
-void get_tasks(task *t1,int n)
+void get_tasks(task *t1, int n)
 {
-	int i=0;
-	while(i<n)
+	int i = 0;
+	while (i < n)
 	{
-	printf("Enter Task %d parameters\n",i+1);
-	printf("Arrival time: ");
-	scanf("%d",&t1->T[arrival]);
-	printf("Execution time: ");
-	scanf("%d",&t1->T[execution]);
-	printf("Deadline time: ");
-	scanf("%d",&t1->T[deadline]);
-	printf("Period: ");
-	scanf("%d",&t1->T[period]);
-	t1->T[abs_arrival]=0;
-	t1->T[execution_copy]=0;
-	t1->T[abs_deadline]=0;
-	t1->instance =0;
-	t1->alive = 0;
-	t1++;
-	i++;
+		printf("Enter Task %d parameters\n", i + 1);
+		printf("Arrival time: ");
+		scanf("%d", &t1->T[arrival]);
+		printf("Execution time: ");
+		scanf("%d", &t1->T[execution]);
+		printf("Deadline time: ");
+		scanf("%d", &t1->T[deadline]);
+		printf("Period: ");
+		scanf("%d", &t1->T[period]);
+		t1->T[abs_arrival] = 0;
+		t1->T[execution_copy] = 0;
+		t1->T[abs_deadline] = 0;
+		t1->instance = 0;
+		t1->alive = 0;
+		t1++;
+		i++;
 	}
 }
 
-int hyperperiod_calc(task *t1,int n)
+int hyperperiod_calc(task *t1, int n)
 {
-	int i=0,ht,a[10];
-	while(i<n)
+	int i = 0, ht, a[10];
+	while (i < n)
 
-		{
-		a[i]=t1->T[period];
+	{
+		a[i] = t1->T[period];
 		t1++;
 		i++;
-		}
-	ht=lcm(a,n);
+	}
+	ht = lcm(a, n);
 
 	return ht;
 }
 
 int gcd(int a, int b)
 {
-  if (b == 0)
-	  return a;
-  else
-  return gcd(b, a%b);
+	if (b == 0)
+		return a;
+	else
+		return gcd(b, a % b);
 }
 
 int lcm(int *a, int n)
 {
-  int res = 1, i;
-  for (i = 0; i < n; i++)
-  {
-    res = res*a[i]/gcd(res, a[i]);
-  }
-  return res;
+	int res = 1, i;
+	for (i = 0; i < n; i++)
+	{
+		res = res * a[i] / gcd(res, a[i]);
+	}
+	return res;
 }
 
-
-
-int sp_interrupt(task *t1,int tmr,int n)
+int sp_interrupt(task *t1, int tmr, int n)
 {
-int i=0,n1=0,a=0;
-task *t1_copy;
-t1_copy=t1;
-	while(i<n)
+	int i = 0, n1 = 0, a = 0;
+	task *t1_copy;
+	t1_copy = t1;
+	while (i < n)
 	{
-		if(tmr == t1->T[abs_arrival])
-			{
+		if (tmr == t1->T[abs_arrival])
+		{
 			t1->alive = 1;
 			a++;
-			}
+		}
 		t1++;
 		i++;
 	}
 
-	t1 =t1_copy;
-	i=0;
+	t1 = t1_copy;
+	i = 0;
 
-	while(i<n)
+	while (i < n)
 	{
-		if(t1->alive==0)
+		if (t1->alive == 0)
 			n1++;
-			t1++;
-			i++;
+		t1++;
+		i++;
 	}
 
-	if(n1==n || a!=0)
+	if (n1 == n || a != 0)
 	{
 		return 1;
 	}
@@ -101,52 +99,50 @@ t1_copy=t1;
 	return 0;
 }
 
-void update_abs_deadline(task *t1,int n,int all)
+void update_abs_deadline(task *t1, int n, int all)
 {
-int i=0;
-		if(all)
-		{
-			while(i<n)
-			{
-				t1->T[abs_deadline] = t1->T[deadline] + t1->T[abs_arrival];
-				t1++;
-				i++;
-			}
-		}
-		else
-		{
-		t1+=n;
-		t1->T[abs_deadline] = t1->T[deadline] + t1->T[abs_arrival];
-		}
-
-}
-
-void update_abs_arrival(task *t1,int n,int k,int all)
-{
-int i=0;
-		if(all)
-		{
-			while(i<n)
-			{
-				t1->T[abs_arrival] = t1->T[arrival] + k*(t1->T[period]);
-				t1++;
-				i++;
-			}
-		}
-		else
-		{
-		t1+=n;
-		t1->T[abs_arrival] = t1->T[arrival] + k* (t1->T[period]);
-		}
-
-}
-
-void copy_execution_time(task *t1,int n,int all)
-{
-	int i=0;
-	if(all)
+	int i = 0;
+	if (all)
 	{
-		while(i<n)
+		while (i < n)
+		{
+			t1->T[abs_deadline] = t1->T[deadline] + t1->T[abs_arrival];
+			t1++;
+			i++;
+		}
+	}
+	else
+	{
+		t1 += n;
+		t1->T[abs_deadline] = t1->T[deadline] + t1->T[abs_arrival];
+	}
+}
+
+void update_abs_arrival(task *t1, int n, int k, int all)
+{
+	int i = 0;
+	if (all)
+	{
+		while (i < n)
+		{
+			t1->T[abs_arrival] = t1->T[arrival] + k * (t1->T[period]);
+			t1++;
+			i++;
+		}
+	}
+	else
+	{
+		t1 += n;
+		t1->T[abs_arrival] = t1->T[arrival] + k * (t1->T[period]);
+	}
+}
+
+void copy_execution_time(task *t1, int n, int all)
+{
+	int i = 0;
+	if (all)
+	{
+		while (i < n)
 		{
 			t1->T[execution_copy] = t1->T[execution];
 			t1++;
@@ -155,21 +151,20 @@ void copy_execution_time(task *t1,int n,int all)
 	}
 	else
 	{
-		t1+=n;
+		t1 += n;
 		t1->T[execution_copy] = t1->T[execution];
 	}
 }
 
-
-int min(task *t1,int n,int p)
+int min(task *t1, int n, int p)
 {
-	int i=0,min=0x7FFF,task_id=IDLE_TASK_ID;
-	while(i<n)
+	int i = 0, min = 0x7FFF, task_id = IDLE_TASK_ID;
+	while (i < n)
 	{
-		if(min>t1->T[p] && t1->alive==1)
+		if (min > t1->T[p] && t1->alive == 1)
 		{
-			min=t1->T[p];
-			task_id=i;
+			min = t1->T[p];
+			task_id = i;
 		}
 		t1++;
 		i++;
@@ -177,18 +172,15 @@ int min(task *t1,int n,int p)
 	return task_id;
 }
 
-
-float cpu_util(task *t1,int n)
+float cpu_util(task *t1, int n)
 {
-	int i=0;
-	float cu=0;
-	while(i<n)
+	int i = 0;
+	float cu = 0;
+	while (i < n)
 	{
-		cu = cu + (float)t1->T[execution]/(float)t1->T[deadline];
+		cu = cu + (float)t1->T[execution] / (float)t1->T[deadline];
 		t1++;
 		i++;
 	}
 	return cu;
-
 }
-
